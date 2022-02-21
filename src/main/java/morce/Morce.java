4b3212d369f396ae48aed7ac6145ae6c591d05dc
IcaroOli <icaroeedu@gmail.com>
2022-02-21 20:14:23 -0300

    Edit

---
+++ src/main/java/morce/Morce.java
@@ -0,0 +1,45 @@
+package morce;
+
+public class Morce {
+
+    public static String morceCodificar(String texto) {
+        int tamanhoT = texto.length();
+        String morse = "   ";
+
+        for (int i = 0; i < tamanhoT; i++) {
+            if (texto.charAt(i) >= 97 && texto.charAt(i) <= 122) {
+                switch (Character.toUpperCase(texto.charAt(i))) {
+                    case 'a' -> morse += ".- ";
+                    case 'b' -> morse += "-... ";
+                    case 'c' -> morse += "-.-. ";
+                    case 'd' -> morse += "-.. ";
+                    case 'e' -> morse += ". ";
+                    case 'f' -> morse += "..-. ";
+                    case 'g' -> morse += "--. ";
+                    case 'h' -> morse += ".... ";
+                    case 'i' -> morse += ".. ";
+                    case 'j' -> morse += ".--- ";
+                    case 'k' -> morse += "-.- ";
+                    case 'l' -> morse += ".-.. ";
+                    case 'm' -> morse += "-- ";
+                    case 'n' -> morse += "-. ";
+                    case 'o' -> morse += "--- ";
+                    case 'p' -> morse += ".--. ";
+                    case 'q' -> morse += "--.- ";
+                    case 'r' -> morse += ".-. ";
+                    case 's' -> morse += "... ";
+                    case 't' -> morse += "- ";
+                    case 'u' -> morse += "..- ";
+                    case 'v' -> morse += "...- ";
+                    case 'w' -> morse += ".-- ";
+                    case 'x' -> morse += "-..- ";
+                    case 'y' -> morse += "-.-- ";
+                    case 'z' -> morse += "--.. ";
+                    case ' ' -> morse += "   ";
+                }
+            }
+        }
+        return morse;
+    }
+
+}
