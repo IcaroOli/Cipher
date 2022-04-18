93d34c5b23eadfca52cdec25e86a05499d3e65ae
IcaroOli <icaroeedu@gmail.com>
2022-04-18 20:33:16 -0300

    Começo test

--- src/main/java/Cesar/Cesar.java
+++ src/main/java/Cesar/Cesar.java
@@ -9,21 +9,23 @@ public class Cesar {
      * @return o texto criptografado
      */
     public static String cesarCriptografar(int chave, String texto) {
+        //Arumar null texto
         StringBuilder textoCifrado = new StringBuilder();
         int tamanhoTexto = texto.length();
+        if (chave <= 26) {
+            for (int i = 0; i < tamanhoTexto; i++) {
+                int charCifrado = ((int) texto.charAt(i)) + chave;
 
-        for (int i = 0; i < tamanhoTexto; i++) {
-            int charCifrado = ((int) texto.charAt(i)) + chave;
-
-            //Letras minusculas na tabela ASII
-            if (texto.charAt(i) >= 97 && texto.charAt(i) <= 122) {
-                charCifrado = (char) ((texto.charAt(i) - 97 + chave) % 26 + 97);
-                //Letras maiuscula na tabela ASII
-            } else if (texto.charAt(i) >= 65 && texto.charAt(i) <= 90)
-                charCifrado = (char) ((texto.charAt(i) - 65 + chave) % 26 + 65);
+                //Letras minusculas na tabela ASII
+                if (texto.charAt(i) >= 97 && texto.charAt(i) <= 122) {
+                    charCifrado = (char) ((texto.charAt(i) - 97 + chave) % 26 + 97);
+                    //Letras maiuscula na tabela ASII
+                } else if (texto.charAt(i) >= 65 && texto.charAt(i) <= 90)
+                    charCifrado = (char) ((texto.charAt(i) - 65 + chave) % 26 + 65);
 
 
-            textoCifrado.append((char) charCifrado);
+                textoCifrado.append((char) charCifrado);
+            }
         }
         return textoCifrado.toString();
     }
@@ -31,15 +33,17 @@ public class Cesar {
     public static String cesarDecriptografia(int chave, String textoCifrado) {
         StringBuilder texto = new StringBuilder();
         int tamanhoTexto = textoCifrado.length();
+        if (chave <= 26) {
+            for (int c = 0; c < tamanhoTexto; c++) {
+                int charDecifrado = (int) textoCifrado.charAt(c) - chave;
 
-        for (int c = 0; c < tamanhoTexto; c++) {
-            int charDecifrado = (int) textoCifrado.charAt(c) - chave;
+                if (textoCifrado.charAt(c) >= 97 && textoCifrado.charAt(c) <= 122) {
+                    charDecifrado = (char) ((textoCifrado.charAt(c) - 97 + chave) % 26 + 97);
+                } else if (textoCifrado.charAt(c) >= 65 && textoCifrado.charAt(c) <= 90)
+                    charDecifrado = (char) ((textoCifrado.charAt(c) - 65 + chave) % 26 + 97);
+                texto.append((char) charDecifrado);
 
-            if (textoCifrado.charAt(c) >= 97 && textoCifrado.charAt(c) <= 122) {
-                charDecifrado = (char) ((textoCifrado.charAt(c) - 97 + chave) % 26 + 97);
-            } else if (textoCifrado.charAt(c) >= 65 && textoCifrado.charAt(c) <= 90)
-                charDecifrado = (char) ((textoCifrado.charAt(c) - 65 + chave) % 26 + 97);
-            texto.append((char) charDecifrado);
+            }
         }
 
         return texto.toString();
---
+++ src/test/java/Cesar/cesarTest.java
@@ -0,0 +1,31 @@
+package Cesar;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static Cesar.Cesar.cesarCriptografar;
+import static Cesar.Cesar.cesarDecriptografia;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+public class cesarTest {
+    @Test
+    public void testSucess_codificarCesar(){
+        int chave = 13;
+        String texto = "icaro";
+        assertEquals("vpneb",cesarCriptografar(chave,texto));
+    }
+    @Test
+    public void testSucess_decodificarCesar(){
+        int chave = 13;
+        String code = "vpneb";
+        assertEquals("icaro",cesarDecriptografia(chave,code));
+    }
+    @Test
+    //Arrumar
+    public void testFail_codificarCesar(){
+        int chave = 15;
+        String texto = "icaro";
+        assertNotEquals("vpneb",cesarCriptografar(chave,texto));
+    }
+}
